65a2e4ee02b75e09a612dff5a888a34bc4dc3e9d
James Chan <jameschanx@gmail.com>
2018-09-16 13:11:23 -0700

    minor tweak to simplify

--- mergesort.py
+++ mergesort.py
@@ -1,12 +1,12 @@
 import numpy as np
 
-
 def mergesort(array):
     if len(array) <= 1:
         return array
-    mid = len(array) // 2
+    mid = len(array)//2
     left = mergesort(array[:mid])
     right = mergesort(array[mid:])
+    
     i, j = 0, 0
     while i < len(left) and j < len(right):
         if left[i] < right[j]:
@@ -15,14 +15,14 @@ def mergesort(array):
         else:
             array[i+j] = right[j]
             j += 1
-    if i < len(left):
-        array[j+i:] = left[i:]
-        i = len(left)
     
-    if j < len(right):
-        array[j+i:] = right[j:]
+    if i < len(left):
+        array[i+j:] = left[i:]
+    else:
+        array[i+j:] = right[j:]
+    
     return array
-        
+
 n = 10
 a = range(n)
 np.random.shuffle(a)
--- quicksort.py
+++ quicksort.py
@@ -1,13 +1,14 @@
 import numpy as np
 
+
 def _partition(array, first, last):
     pivot_val = array[first]
     i = first
-    for j in range(i, last):
+    for j in range(first, last):
         if array[j] < pivot_val:
             i += 1
             array[i], array[j] = array[j], array[i]
-    array[first], array[i] = array[i], array[first]
+    array[i], array[first] = array[first], array[i]
     return i
 
 def _quicksort(array, first, last):
@@ -15,12 +16,12 @@ def _quicksort(array, first, last):
         return
     pivot = _partition(array, first, last)
     _quicksort(array, first, pivot)
-    _quicksort(array, pivot+1, last)
-    
+    _quicksort(array, pivot+1, last)        
+
 def quicksort(array):
     _quicksort(array, 0, len(array))
 
-n = 7
+n = 15
 a = range(n)
 np.random.shuffle(a)
 print(a)
